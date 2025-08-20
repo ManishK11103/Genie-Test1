1ea9c2a29c39007f766f5a002afd3622ca780ae9
ManishK11103 <manikot@gmail.com>
2025-08-20 13:08:27 +0000

    test

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,5 +1,5 @@
 /// <reference types="vite/client" />
-    declare module '/genieApi' {
+    declare module './genieApi.js' {
         const genieApi: any; // Or more specific types if known
         export default genieApi;
     }
